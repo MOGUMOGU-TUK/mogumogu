14ec06ba18798a637c8be86df98453043d1c89cb
ryeoeunn <fudms@tukorea.ac.kr>
2026-07-03 13:44:46 +0900

    fix: AGP/Gradle 버전 config plugin으로 고정

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,18 +1,54 @@
-const { expo } = require("./app.json");
+const {
+  withProjectBuildGradle,
+  withDangerousMod,
+} = require("@expo/config-plugins");
+const path = require("path");
+const fs = require("fs");
 
-module.exports = {
-  expo: {
-    ...expo,
+const withPinnedAGP = (config) => {
+  return withProjectBuildGradle(config, (config) => {
+    config.modResults.contents = config.modResults.contents.replace(
+      "classpath('com.android.tools.build:gradle')",
+      "classpath('com.android.tools.build:gradle:8.7.2')"
+    );
+    return config;
+  });
+};
+
+const withGradleWrapper = (config) => {
+  return withDangerousMod(config, [
+    "android",
+    (config) => {
+      const filePath = path.join(
+        config.modRequest.platformProjectRoot,
+        "gradle/wrapper/gradle-wrapper.properties"
+      );
+      let contents = fs.readFileSync(filePath, "utf-8");
+      contents = contents.replace(
+        /distributionUrl=.*gradle-.*-bin\.zip/,
+        "distributionUrl=https\\://services.gradle.org/distributions/gradle-8.10.2-bin.zip"
+      );
+      fs.writeFileSync(filePath, contents);
+      return config;
+    },
+  ]);
+};
+
+module.exports = ({ config }) => {
+  const updated = {
+    ...config,
     android: {
-      ...expo.android,
+      ...config.android,
       googleServicesFile:
         process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
     },
     ios: {
-      ...expo.ios,
+      ...config.ios,
       googleServicesFile:
         process.env.GOOGLE_SERVICES_JSON_IOS ?? "./GoogleService-Info.plist",
     },
-    plugins: [...(expo.plugins ?? []), "expo-build-properties"],
-  },
+    plugins: [...(config.plugins ?? []), "expo-build-properties"],
+  };
+
+  return withGradleWrapper(withPinnedAGP(updated));
 };
